af11c12dc5de1dfb4228eb688d6a0a030032387e
YuDavidCao <yu.cao20041208@gmail.com>
2024-10-07 14:07:52 -0400

    added meeting summary and post meetings for Oct 5

---
+++ LinearPlot3D/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { createRef } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState([]);
+  const inputRef = createRef();
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <input
+        ref={inputRef}
+        onChange={() => console.log(inputRef.current.value)}
+      ></input>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => [...count, 9])}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
