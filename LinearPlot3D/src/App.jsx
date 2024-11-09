3deeaddc1035990c6c8e2464bc44aee3ccb412f9
YuDavidCao <yu.cao20041208@gmail.com>
2024-11-09 14:08:21 -0500

    implemented mechanism to add and delete vectors (stored in an array)

--- LinearPlot3D/src/App.jsx
+++ LinearPlot3D/src/App.jsx
@@ -1,136 +1,92 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Line, Text } from "@react-three/drei";
+import { Line } from "@react-three/drei";
 import * as THREE from "three";
 
 const origin = new THREE.Vector3(0, 0, 0);
 
+class Vec {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
+
 function App() {
+  const [vectors, setVectors] = useState([]);
+  const handleAddVector = () => {
+    setVectors([...vectors, new Vec(0, 0, 0)]);
+  };
+
+  const handleDeleteVector = () => {
+    setVectors(vectors.slice(0, vectors.length - 1));
+  };
+
   const coordX = new THREE.Vector3(5, 0, 0);
   const coordY = new THREE.Vector3(0, 5, 0);
-
-  const [line1, setLine1] = useState([origin, origin]);
-
-  const [line2, setLine2] = useState([origin, origin]);
-
-  const [line3, setLine3] = useState([origin, origin]);
-
-  const inputRef1 = useRef(null);
-  const inputRef2 = useRef(null);
-  const inputRef3 = useRef(null);
-  const inputRef4 = useRef(null);
-  const inputRef5 = useRef(null);
-  const inputRef6 = useRef(null);
-
-  const handleInput1Change = () => {
-    setLine1([
-      line1[0],
-      new THREE.Vector3(
-        Number(inputRef1.current.value),
-        line1[1].y,
-        line1[1].z
-      ),
-    ]);
-  };
-
-  const handleInput2Change = () => {
-    setLine1([
-      line1[0],
-      new THREE.Vector3(
-        line1[1].x,
-        Number(inputRef2.current.value),
-        line1[1].z
-      ),
-    ]);
-  };
-
-  const handleInput3Change = () => {
-    setLine2([
-      line2[0],
-      new THREE.Vector3(
-        Number(inputRef3.current.value),
-        line2[1].y,
-        line2[1].z
-      ),
-    ]);
-  };
-
-  const handleInput4Change = () => {
-    setLine2([
-      line2[0],
-      new THREE.Vector3(
-        line2[1].x,
-        Number(inputRef4.current.value),
-        line2[1].z
-      ),
-    ]);
-  };
-
-  const handleInput5Change = () => {
-    setLine3([
-      line3[0],
-      new THREE.Vector3(
-        Number(inputRef5.current.value),
-        line3[1].y,
-        line3[1].z
-      ),
-    ]);
-  };
-
-  const handleInput6Change = () => {
-    setLine3([
-      line3[0],
-      new THREE.Vector3(
-        line3[1].x,
-        Number(inputRef6.current.value),
-        line3[1].z
-      ),
-    ]);
-  };
 
   return (
     <>
       <div className="app">
         <div className="side">
-          <div className="input-box">
-            <div>x1</div>
-            <input ref={inputRef1} onChange={handleInput1Change}></input>
+          <div className="scroll-div">
+            {vectors.map((vector, index) => (
+              <div key={index} className="input-box">
+                <div>x</div>
+                <input
+                  onChange={(e) => {
+                    setVectors([
+                      ...vectors.slice(0, index),
+                      new Vec(
+                        Number(e.target.value),
+                        vectors[index].y,
+                        vectors[index].z
+                      ),
+                      ...vectors.slice(index + 1),
+                    ]);
+                  }}
+                ></input>
+                <div>y</div>
+                <input
+                  onChange={(e) => {
+                    setVectors([
+                      ...vectors.slice(0, index),
+                      new Vec(
+                        vectors[index].x,
+                        Number(e.target.value),
+                        vectors[index].z
+                      ),
+                      ...vectors.slice(index + 1),
+                    ]);
+                  }}
+                ></input>
+              </div>
+            ))}
           </div>
-          <div className="input-box">
-            <div>y1</div>
-            <input ref={inputRef2} onChange={handleInput2Change}></input>
-          </div>
-          <div className="input-box">
-            <div>x2</div>
-            <input ref={inputRef3} onChange={handleInput3Change}></input>
-          </div>
-          <div className="input-box">
-            <div>y2</div>
-            <input ref={inputRef4} onChange={handleInput4Change}></input>
-          </div>
-          <div className="input-box">
-            <div>x3</div>
-            <input ref={inputRef5} onChange={handleInput5Change}></input>
-          </div>
-          <div className="input-box">
-            <div>y3</div>
-            <input ref={inputRef6} onChange={handleInput6Change}></input>
-          </div>
+          <button className="vector-button" onClick={handleAddVector}>
+            Add a vector
+          </button>
+          <button className="vector-button" onClick={handleDeleteVector}>
+            Delete a vector
+          </button>
         </div>
-        <div className="test">
+        <div className="canvas-bound">
           <Canvas>
-            <Text
-              position={[0, 0, 0]} // Position in 3D space
-              color="black" // Color of the text
-            >
-              Hello, React Three.js!
-            </Text>
             <Line points={[origin, coordX]} color="black"></Line>
             <Line points={[origin, coordY]} color="black"></Line>
-            <Line points={line1} color="blue" lineWidth={4} />
-            <Line points={line2} color="red" lineWidth={4} />
-            <Line points={line3} color="green" lineWidth={4} />
+            {vectors.map((vector, index) => (
+              <Line
+                key={index}
+                points={[
+                  origin,
+                  new THREE.Vector3(vector.x, vector.y, vector.z),
+                ]}
+                color="purple"
+                lineWidth={4}
+              />
+            ))}
           </Canvas>
         </div>
       </div>
---
+++ LinearPlot3D/src/copy.jsx
@@ -0,0 +1,137 @@
+import { useRef, useState } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Line, Text } from "@react-three/drei";
+import * as THREE from "three";
+
+const origin = new THREE.Vector3(0, 0, 0);
+
+function App() {
+  const coordX = new THREE.Vector3(5, 0, 0);
+  const coordY = new THREE.Vector3(0, 5, 0);
+
+  const [line1, setLine1] = useState([origin, origin]);
+
+  const [line2, setLine2] = useState([origin, origin]);
+
+  const [line3, setLine3] = useState([origin, origin]);
+
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
+  const inputRef4 = useRef(null);
+  const inputRef5 = useRef(null);
+  const inputRef6 = useRef(null);
+
+  const handleInput1Change = () => {
+    setLine1([
+      line1[0],
+      new THREE.Vector3(
+        Number(inputRef1.current.value),
+        line1[1].y,
+        line1[1].z
+      ),
+    ]);
+  };
+
+  const handleInput2Change = () => {
+    setLine1([
+      line1[0],
+      new THREE.Vector3(
+        line1[1].x,
+        Number(inputRef2.current.value),
+        line1[1].z
+      ),
+    ]);
+  };
+
+  const handleInput3Change = () => {
+    setLine2([
+      line2[0],
+      new THREE.Vector3(
+        Number(inputRef3.current.value),
+        line2[1].y,
+        line2[1].z
+      ),
+    ]);
+  };
+
+  const handleInput4Change = () => {
+    setLine2([
+      line2[0],
+      new THREE.Vector3(
+        line2[1].x,
+        Number(inputRef4.current.value),
+        line2[1].z
+      ),
+    ]);
+  };
+
+  const handleInput5Change = () => {
+    setLine3([
+      line3[0],
+      new THREE.Vector3(
+        Number(inputRef5.current.value),
+        line3[1].y,
+        line3[1].z
+      ),
+    ]);
+  };
+
+  const handleInput6Change = () => {
+    setLine3([
+      line3[0],
+      new THREE.Vector3(
+        line3[1].x,
+        Number(inputRef6.current.value),
+        line3[1].z
+      ),
+    ]);
+  };
+
+  return (
+    <>
+      <div className="app">
+        <div className="side">
+          <div class="scroll-div">
+            <div className="input-box">
+              <div>x1</div>
+              <input ref={inputRef1} onChange={handleInput1Change}></input>
+            </div>
+            <div className="input-box">
+              <div>y1</div>
+              <input ref={inputRef2} onChange={handleInput2Change}></input>
+            </div>
+            <div className="input-box">
+              <div>x2</div>
+              <input ref={inputRef3} onChange={handleInput3Change}></input>
+            </div>
+            <div className="input-box">
+              <div>y2</div>
+              <input ref={inputRef4} onChange={handleInput4Change}></input>
+            </div>
+            <div className="input-box">
+              <div>x3</div>
+              <input ref={inputRef5} onChange={handleInput5Change}></input>
+            </div>
+            <div className="input-box">
+              <div>y3</div>
+              <input ref={inputRef6} onChange={handleInput6Change}></input>
+            </div>
+          </div>
+          <div className="test">
+            <Canvas>
+              <Line points={[origin, coordX]} color="black"></Line>
+              <Line points={[origin, coordY]} color="black"></Line>
+              <Line points={line1} color="blue" lineWidth={4} />
+              <Line points={line2} color="red" lineWidth={4} />
+              <Line points={line3} color="green" lineWidth={4} />
+            </Canvas>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
