70aaaee6bbe8282f57503b47bfaee5a707294fa5
YuDavidCao <yu.cao20041208@gmail.com>
2025-02-25 10:32:01 -0500

    code refactor on matrix input

--- LinearPlot3D/src/App.jsx
+++ LinearPlot3D/src/App.jsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { v4 as uuidv4 } from "uuid";
-import { Github } from "lucide-react";
 
 const origin = new THREE.Vector3(0, 0, 0);
 
@@ -17,6 +16,9 @@ function App() {
 
   const [showAnimation, setShowAnimation] = useState(true);
   const [showVectorLabels, setShowVectorLabels] = useState(true);
+
+  const numOfRow = 3;
+  const numOfCol = 3;
 
   const updateAnimSpeed = (e) => {
     setAnimSpeed(e.target.value);
@@ -36,77 +38,6 @@ function App() {
 
   const handleColorChange = (e) => {
     setColors(e.target.value);
-  };
-
-  const handleMatrixa11Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 0),
-      Number(e.target.value),
-      ...matrix.slice(1),
-    ]);
-  };
-
-  const handleMatrixa12Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 1),
-      Number(e.target.value),
-      ...matrix.slice(2),
-    ]);
-  };
-
-  const handleMatrixa13Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 2),
-      Number(e.target.value),
-      ...matrix.slice(3),
-    ]);
-  };
-
-  const handleMatrixa21Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 3),
-      Number(e.target.value),
-      ...matrix.slice(4),
-    ]);
-  };
-
-  const handleMatrixa22Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 4),
-      Number(e.target.value),
-      ...matrix.slice(5),
-    ]);
-  };
-
-  const handleMatrixa23Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 5),
-      Number(e.target.value),
-      ...matrix.slice(6),
-    ]);
-  };
-  const handleMatrixa31Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 6),
-      Number(e.target.value),
-      ...matrix.slice(7),
-    ]);
-  };
-
-  const handleMatrixa32Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 7),
-      Number(e.target.value),
-      ...matrix.slice(8),
-    ]);
-  };
-
-  const handleMatrixa33Change = (e) => {
-    setMatrix([
-      ...matrix.slice(0, 8),
-      Number(e.target.value),
-      ...matrix.slice(9),
-    ]);
   };
 
   const coordX = new THREE.Vector3(5, 0, 0);
@@ -134,57 +65,34 @@ function App() {
           <hr className="divider-h"></hr>
           <div className="matrix-input">
             <div className="matrix-title">Transformation Matrix</div>
-            <div className="matrix-row">
-              <input
-                placeholder="1"
-                onChange={handleMatrixa11Change}
-                className="matrix-input-box"
-              ></input>
-              <input
-                placeholder="0"
-                onChange={handleMatrixa12Change}
-                className="matrix-input-box"
-              ></input>
-              <input
-                placeholder="0"
-                onChange={handleMatrixa13Change}
-                className="matrix-input-box"
-              ></input>
-            </div>
-            <div className="matrix-row">
-              <input
-                placeholder="0"
-                onChange={handleMatrixa21Change}
-                className="matrix-input-box"
-              ></input>
-              <input
-                placeholder="1"
-                onChange={handleMatrixa22Change}
-                className="matrix-input-box"
-              ></input>
-              <input
-                placeholder="0"
-                onChange={handleMatrixa23Change}
-                className="matrix-input-box"
-              ></input>
-            </div>
-            <div className="matrix-row">
-              <input
-                placeholder="0"
-                onChange={handleMatrixa31Change}
-                className="matrix-input-box"
-              ></input>
-              <input
-                placeholder="0"
-                onChange={handleMatrixa32Change}
-                className="matrix-input-box"
-              ></input>
-              <input
-                placeholder="1"
-                onChange={handleMatrixa33Change}
-                className="matrix-input-box"
-              ></input>
-            </div>
+            {Array.from({ length: numOfRow }).map((_, rowIndex) => {
+              return (
+                <div key={rowIndex} className="matrix-row">
+                  {Array.from({ length: numOfCol }).map((_, colIndex) => {
+                    return (
+                      <input
+                        key={colIndex}
+                        placeholder={rowIndex === colIndex ? "1" : "0"}
+                        onChange={(e) => {
+                          let val = e.target.value;
+                          if (isNaN(parseFloat(val))) {
+                            val = 0;
+                          }
+                          const index = rowIndex * numOfRow + colIndex;
+                          let newMatrix = [
+                            ...matrix.slice(0, index),
+                            Number(val),
+                            ...matrix.slice(index + 1),
+                          ];
+                          setMatrix(newMatrix);
+                        }}
+                        className="matrix-input-box"
+                      ></input>
+                    );
+                  })}
+                </div>
+              );
+            })}
           </div>
           <hr className="divider-h"></hr>
           <div className="vector-input">
